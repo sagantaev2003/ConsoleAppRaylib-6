0f1d359636b36d39ddf4610687ccc52d145b16cc
sagantaev2003 <156180053+sagantaev2003@users.noreply.github.com>
2025-03-11 13:02:08 +0500

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,142 @@
+﻿#include "raylib.h"
+#include <stdlib.h>
+#include <time.h>
+
+typedef enum { ROCK, PAPER, SCISSORS, NONE } Choice;
+
+Choice GetComputerChoice() {
+    return (Choice)(rand() % 3);
+}
+
+const char* GetResult(Choice player, Choice computer, Color* textColor) {
+    if (player == computer) {
+        *textColor = ORANGE;
+        return "Draw!";
+    }
+    if ((player == ROCK && computer == SCISSORS) ||
+        (player == PAPER && computer == ROCK) ||
+        (player == SCISSORS && computer == PAPER)) {
+        *textColor = GREEN;
+        return "You Win!";
+    }
+    *textColor = RED;
+    return "You Lose!";
+}
+
+int main() {
+    int screenWidth = 1000;
+    int screenHeight = 600;
+    InitWindow(screenWidth, screenHeight, "Rock, Paper, Scissors - Sagantay Adil CS-104(s)");
+    SetTargetFPS(60);
+    srand(static_cast<unsigned int>(time(NULL)));
+
+    Texture2D rockSprite = LoadTexture("resources/rock.png");
+    Texture2D paperSprite = LoadTexture("resources/paper.png");
+    Texture2D scissorsSprite = LoadTexture("resources/scissors.png");
+
+    Choice playerChoice = NONE;
+    Choice computerChoice = NONE;
+    bool gameStarted = false;
+    bool selectionMade = false;
+    Color resultColor = BLACK;
+
+    Rectangle rockButton = { screenWidth / 2 - 225, 200, 100, 50 };
+    Rectangle paperButton = { screenWidth / 2 - 50, 200, 100, 50 };
+    Rectangle scissorsButton = { screenWidth / 2 + 125, 200, 120, 50 };
+    Rectangle resetButton = { screenWidth / 2 - 75, 300, 150, 50 };
+
+    float cornerRadius = 0.4f;  // Радиус округления кнопок
+
+    while (!WindowShouldClose()) {
+        Vector2 mousePoint = GetMousePosition();
+
+        bool hoveredRock = CheckCollisionPointRec(mousePoint, rockButton);
+        bool hoveredPaper = CheckCollisionPointRec(mousePoint, paperButton);
+        bool hoveredScissors = CheckCollisionPointRec(mousePoint, scissorsButton);
+        bool hoveredReset = CheckCollisionPointRec(mousePoint, resetButton);
+
+        if (IsKeyPressed(KEY_SPACE) || (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && hoveredReset)) {
+            playerChoice = NONE;
+            computerChoice = NONE;
+            gameStarted = false;
+            selectionMade = false;
+            resultColor = BLACK;
+        }
+
+        if (!gameStarted && !selectionMade) {
+            if (IsKeyPressed(KEY_R)) playerChoice = ROCK;
+            if (IsKeyPressed(KEY_P)) playerChoice = PAPER;
+            if (IsKeyPressed(KEY_S)) playerChoice = SCISSORS;
+
+            if (playerChoice != NONE) {
+                computerChoice = GetComputerChoice();
+                gameStarted = true;
+                selectionMade = true;
+            }
+        }
+
+        if (!selectionMade && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            if (hoveredRock) playerChoice = ROCK;
+            if (hoveredPaper) playerChoice = PAPER;
+            if (hoveredScissors) playerChoice = SCISSORS;
+
+            if (playerChoice != NONE) {
+                computerChoice = GetComputerChoice();
+                gameStarted = true;
+                selectionMade = true;
+            }
+        }
+
+        BeginDrawing();
+        ClearBackground(GRAY);
+
+        DrawText("Press R - Rock, P - Paper, S - Scissors to play or click buttons", screenWidth / 2 - 300, 150, 20, BLACK);
+        DrawText("Press SPACE or click reset to restart", screenWidth / 2 - 200, 265, 20, BLACK);
+
+        Color buttonColor = LIGHTGRAY;
+        Color hoverColor = DARKGRAY;
+
+        // Рисуем кнопки с округлением
+        DrawRectangleRounded(rockButton, cornerRadius, 6, hoveredRock ? hoverColor : buttonColor);
+        DrawRectangleRounded(paperButton, cornerRadius, 6, hoveredPaper ? hoverColor : buttonColor);
+        DrawRectangleRounded(scissorsButton, cornerRadius, 6, hoveredScissors ? hoverColor : buttonColor);
+        DrawRectangleRounded(resetButton, cornerRadius, 6, hoveredReset ? hoverColor : buttonColor);
+
+        DrawText("Rock", rockButton.x + 25, rockButton.y + 15, 20, BLACK);
+        DrawText("Paper", paperButton.x + 25, paperButton.y + 15, 20, BLACK);
+        DrawText("Scissors", scissorsButton.x + 15, scissorsButton.y + 15, 20, BLACK);
+        DrawText("Reset", resetButton.x + 40, resetButton.y + 15, 20, BLACK);
+
+        if (gameStarted) {
+            DrawText("Your choice:", screenWidth / 2 - 200, 350, 20, BLACK);
+            DrawText("Computer choice:", screenWidth / 2 + 50, 350, 20, BLACK);
+
+            Vector2 playerPos = { screenWidth / 2 - 200, 380 };
+            Vector2 computerPos = { screenWidth / 2 + 50, 380 };
+
+            if (playerChoice == ROCK)
+                DrawTexturePro(rockSprite, { 0, 0, (float)rockSprite.width, (float)rockSprite.height }, { playerPos.x, playerPos.y, 100, 100 }, { 0, 0 }, 0, WHITE);
+            if (playerChoice == PAPER)
+                DrawTexturePro(paperSprite, { 0, 0, (float)paperSprite.width, (float)paperSprite.height }, { playerPos.x, playerPos.y, 100, 100 }, { 0, 0 }, 0, WHITE);
+            if (playerChoice == SCISSORS)
+                DrawTexturePro(scissorsSprite, { 0, 0, (float)scissorsSprite.width, (float)scissorsSprite.height }, { playerPos.x, playerPos.y, 100, 100 }, { 0, 0 }, 0, WHITE);
+
+            if (computerChoice == ROCK)
+                DrawTexturePro(rockSprite, { 0, 0, (float)rockSprite.width, (float)rockSprite.height }, { computerPos.x, computerPos.y, 100, 100 }, { 0, 0 }, 0, WHITE);
+            if (computerChoice == PAPER)
+                DrawTexturePro(paperSprite, { 0, 0, (float)paperSprite.width, (float)paperSprite.height }, { computerPos.x, computerPos.y, 100, 100 }, { 0, 0 }, 0, WHITE);
+            if (computerChoice == SCISSORS)
+                DrawTexturePro(scissorsSprite, { 0, 0, (float)scissorsSprite.width, (float)scissorsSprite.height }, { computerPos.x, computerPos.y, 100, 100 }, { 0, 0 }, 0, WHITE);
+
+            DrawText(GetResult(playerChoice, computerChoice, &resultColor), screenWidth / 2 - 50, 500, 30, resultColor);
+        }
+
+        EndDrawing();
+    }
+
+    UnloadTexture(rockSprite);
+    UnloadTexture(paperSprite);
+    UnloadTexture(scissorsSprite);
+    CloseWindow();
+    return 0;
+}
---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,129 @@
+﻿#include "raylib.h"
+#include <stdlib.h>
+#include <time.h>
+
+typedef enum { ROCK, PAPER, SCISSORS, NONE } Choice;
+
+const char* DetermineWinner(Choice player1, Choice player2, Color* textColor) {
+    if (player1 == player2) {
+        *textColor = ORANGE;
+        return "Draw!";
+    }
+    if ((player1 == ROCK && player2 == SCISSORS) ||
+        (player1 == PAPER && player2 == ROCK) ||
+        (player1 == SCISSORS && player2 == PAPER)) {
+        *textColor = GREEN;
+        return "Player 1 Wins!";
+    }
+    *textColor = RED;
+    return "Player 2 Wins!";
+}
+
+int main() {
+    int screenWidth = 1000;
+    int screenHeight = 600;
+    InitWindow(screenWidth, screenHeight, "Rock, Paper, Scissors - Two Players");
+    SetTargetFPS(60);
+
+    Texture2D rockSprite = LoadTexture("resources/rock.png");
+    Texture2D paperSprite = LoadTexture("resources/paper.png");
+    Texture2D scissorsSprite = LoadTexture("resources/scissors.png");
+
+    Choice player1Choice = NONE;
+    Choice player2Choice = NONE;
+    bool gameStarted = false;
+    bool player1MadeChoice = false;
+    bool player2MadeChoice = false;
+    Color resultColor = BLACK;
+
+    Rectangle resetButton = { screenWidth / 2 - 75, 300, 150, 50 };
+    float cornerRadius = 0.4f;
+
+    while (!WindowShouldClose()) {
+        Vector2 mousePoint = GetMousePosition();
+        bool hoveredReset = CheckCollisionPointRec(mousePoint, resetButton);
+
+        if (IsKeyPressed(KEY_SPACE) || (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && hoveredReset)) {
+            player1Choice = NONE;
+            player2Choice = NONE;
+            gameStarted = false;
+            player1MadeChoice = false;
+            player2MadeChoice = false;
+            resultColor = BLACK;
+        }
+
+        if (!gameStarted) {
+            if (!player1MadeChoice) {
+                if (IsKeyPressed(KEY_R)) { player1Choice = ROCK; player1MadeChoice = true; }
+                if (IsKeyPressed(KEY_P)) { player1Choice = PAPER; player1MadeChoice = true; }
+                if (IsKeyPressed(KEY_S)) { player1Choice = SCISSORS; player1MadeChoice = true; }
+            }
+            if (player1MadeChoice && !player2MadeChoice) {
+                if (IsKeyPressed(KEY_A)) { player2Choice = ROCK; player2MadeChoice = true; }
+                if (IsKeyPressed(KEY_W)) { player2Choice = PAPER; player2MadeChoice = true; }
+                if (IsKeyPressed(KEY_D)) { player2Choice = SCISSORS; player2MadeChoice = true; }
+            }
+
+            if (player1MadeChoice && player2MadeChoice) {
+                gameStarted = true;
+            }
+        }
+
+        BeginDrawing();
+        ClearBackground(GRAY);
+
+        DrawText("Player 1: Press R - Rock, P - Paper, S - Scissors", 50, 150, 20, BLACK);
+        DrawText("Player 2: Press A - Rock, W - Paper, D - Scissors", 50, 180, 20, BLACK);
+        DrawText("Press SPACE or click reset to restart", 50, 210, 20, BLACK);
+
+        if (!player1MadeChoice) {
+            DrawText("Player 1, make your choice!", 50, 250, 20, RED);
+        }
+        else if (!gameStarted) {
+            DrawText("Player 1 has chosen!", 50, 250, 20, GREEN);
+        }
+
+        if (player1MadeChoice && !player2MadeChoice) {
+            DrawText("Player 2, make your choice!", 50, 280, 20, RED);
+        }
+
+        if (player1MadeChoice && player2MadeChoice) {
+            DrawText("Both players have chosen!", 50, 310, 20, GREEN);
+        }
+
+        DrawRectangleRounded(resetButton, cornerRadius, 6, hoveredReset ? DARKGRAY : LIGHTGRAY);
+        DrawText("Reset", resetButton.x + 40, resetButton.y + 15, 20, BLACK);
+
+        if (gameStarted) {
+            DrawText("Player 1's choice:", 200, 350, 20, BLACK);
+            DrawText("Player 2's choice:", 600, 350, 20, BLACK);
+
+            Vector2 player1Pos = { 200, 380 };
+            Vector2 player2Pos = { 600, 380 };
+
+            if (player1Choice == ROCK)
+                DrawTexturePro(rockSprite, { 0, 0, (float)rockSprite.width, (float)rockSprite.height }, { player1Pos.x, player1Pos.y, 100, 100 }, { 0, 0 }, 0, WHITE);
+            if (player1Choice == PAPER)
+                DrawTexturePro(paperSprite, { 0, 0, (float)paperSprite.width, (float)paperSprite.height }, { player1Pos.x, player1Pos.y, 100, 100 }, { 0, 0 }, 0, WHITE);
+            if (player1Choice == SCISSORS)
+                DrawTexturePro(scissorsSprite, { 0, 0, (float)scissorsSprite.width, (float)scissorsSprite.height }, { player1Pos.x, player1Pos.y, 100, 100 }, { 0, 0 }, 0, WHITE);
+
+            if (player2Choice == ROCK)
+                DrawTexturePro(rockSprite, { 0, 0, (float)rockSprite.width, (float)rockSprite.height }, { player2Pos.x, player2Pos.y, 100, 100 }, { 0, 0 }, 0, WHITE);
+            if (player2Choice == PAPER)
+                DrawTexturePro(paperSprite, { 0, 0, (float)paperSprite.width, (float)paperSprite.height }, { player2Pos.x, player2Pos.y, 100, 100 }, { 0, 0 }, 0, WHITE);
+            if (player2Choice == SCISSORS)
+                DrawTexturePro(scissorsSprite, { 0, 0, (float)scissorsSprite.width, (float)scissorsSprite.height }, { player2Pos.x, player2Pos.y, 100, 100 }, { 0, 0 }, 0, WHITE);
+
+            DrawText(DetermineWinner(player1Choice, player2Choice, &resultColor), screenWidth / 2 - 50, 500, 30, resultColor);
+        }
+
+        EndDrawing();
+    }
+
+    UnloadTexture(rockSprite);
+    UnloadTexture(paperSprite);
+    UnloadTexture(scissorsSprite);
+    CloseWindow();
+    return 0;
+}
